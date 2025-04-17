51d2a0886d9e3c909f317c8784f13b8524643ebe
Davi Oliveira <davioliveiraes7@gmail.com>
2025-04-16 22:01:26 -0300

    feat: Implementing check_in routes

--- src/models/repository/check_ins_repository.py
+++ src/models/repository/check_ins_repository.py
@@ -7,7 +7,7 @@ class CheckInRepository:
       with db_connection_handler as database:
          try:
             check_in = (
-               CheckIns(attendee_id=attendee_id)
+               CheckIns(attendeeId=attendee_id)
             )
             database.session.add(check_in)
             database.session.commit()

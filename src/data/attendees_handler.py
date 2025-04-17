bbe0682c97f49b60731b9a2be8d4789212b83a2b
Davi Oliveira <davioliveiraes7@gmail.com>
2025-04-17 06:21:29 -0300

    feat: Implementing Handle errors in project

--- src/data/attendees_handler.py
+++ src/data/attendees_handler.py
@@ -3,7 +3,8 @@ from src.models.repository.attendees_repository import AttendeesRepository
 from src.models.repository.events_repository import EventsRepository
 from src.http_types.http_request import HttpRequest
 from src.http_types.http_response import HttpResponse
-
+from src.errors.error_types.http_not_found import HttpNotFoundError
+from src.errors.error_types.http_conflict import HttpConflictError
 
 class AttendeesHandler:
     def __init__(self) -> None:
@@ -17,7 +18,7 @@ class AttendeesHandler:
         event_attendees_count = self.__events_repository.count_event_attendees(event_id)
 
         if (event_attendees_count["attendeesAmount"] and event_attendees_count['maximumAttendees'] < event_attendees_count['attendeesAmount']):
-            raise Exception("Evento Lotado.")
+            raise HttpConflictError("Evento Lotado.")
 
         body["uuid"] = str(uuid.uuid4())
         body["event_id"] = event_id
@@ -30,7 +31,7 @@ class AttendeesHandler:
         badge = self.__attendees_repository.get_attendee_badge_by_id(attendee_id)
 
         if not badge:
-            raise Exception("Participante não encontrado")
+            raise HttpNotFoundError("Participante não encontrado")
 
         return HttpResponse(
             body={
@@ -47,7 +48,7 @@ class AttendeesHandler:
         event_id = http_request.param["event_id"]
         attendees = self.__attendees_repository.get_attendees_by_event_id(event_id)
         if not attendees:
-            raise Exception("Participantes não encontrados")
+            raise HttpNotFoundError("Participantes não encontrados")
 
         formatted_attendees = []
         for atteendee in attendees:
